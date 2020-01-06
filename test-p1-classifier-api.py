83e1697a08cf702087f31db45339ee05dd90cae4
Kishore Iyer <kishore.ramachandran@igolgi.com>
2020-01-06 13:57:26 -0600

    Minor changes to HTTP client script for the classifier API

--- test-p1-classifier-api.py
+++ test-p1-classifier-api.py
@@ -7,7 +7,8 @@ post_json = {
     #"input": "http://10.1.10.190/image1000000_9.png",
     #"input": "http://10.1.10.110:8080/openoutputs/image1000000_65.png",
     #"input": "http://10.1.10.110:8080/openoutputs/image1000000_1.png",
-    "input": "http://10.1.10.110:8080/openoutputs/classifier_input/images/test_0215.jpg",
+    #"input": "http://10.1.10.110:8080/openoutputs/classifier_input/images/test_0215.jpg",
+    "input": "http://10.1.10.190/classify/image2000000_1.png",
     "type": "stream",
     "output_dir": "/mnt/bigdrive1/cnn/outputs/",
     "config": {"detection_threshold": 0.50},
@@ -18,7 +19,8 @@ custom_headers = {'content-type': 'application/json'}
 
 def send_post_request(input_img=None):
     if input_img:
-        post_json["input"] = "http://10.1.10.110:8080/openoutputs/classifier_input/images/" + input_img
+        #post_json["input"] = "http://10.1.10.110:8080/openoutputs/classifier_input/images/" + input_img
+        post_json["input"] = "http://10.1.10.190/classify/" + input_img
         
     pprint.pprint(post_json)
     post_json_dumps = json.dumps(post_json)
@@ -53,6 +55,9 @@ def send_post_request(input_img=None):
                 break
             else:
                 #print("Retrying POST")
+                response_json = response.json()
+                #response_json_id = response_json["id"]
+                print("POST RESPONSE: ", response_json)                
                 break
             
         if (retval == -1):
@@ -84,7 +89,8 @@ def send_get_request(response_id):
             print("Ambiguous error for URL: " + str(url_name))
             retval = -1
         else:
-            print(response_get.status_code)
+            print("Size in bytes for response: ", len(response_get.content))
+            print("GET response status code: ", response_get.status_code)
             response_json = response_get.json()        
             print(response_json)
             if (response_json["status"] == "finished"):
@@ -101,20 +107,23 @@ def send_get_request(response_id):
 if __name__ == "__main__":
     response_id_list = []
 
-    response_id = send_post_request()
-    response_id_list.append(response_id)
+    print(" About to send post request")
+    #response_id = send_post_request()
+    #response_id_list.append(response_id)
 
-    for i in range(1,2187):
+    for i in range(1,20):
         print(" * i = ", i)
-        response_id = send_post_request("test_%04d" % i + ".jpg")
+        #response_id = send_post_request("test_%04d" % i + ".jpg")
+        response_id = send_post_request("image1000000_%d" % i + ".png")
         response_id_list.append(response_id)
-        time.sleep(0.05) # 50ms sleep
-        if (i % 20) == 0:
+        #time.sleep(1.0) # 50ms sleep
+        if (i % 30) == 0:
             for response_id in response_id_list:
                 if response_id >= 0:
                     send_get_request(response_id)
             response_id_list = []
-    
+
+    #time.sleep(1.0)
     for response_id in response_id_list:
         if response_id >= 0:
             send_get_request(response_id)
