d00d795f5bf777f388c2200e2278bc90ad7fc65a
Kishore Iyer <kishore.ramachandran@igolgi.com>
2020-01-10 12:36:48 -0600

    Changes to get gunicorn running in one docker and the core application in another

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,12 @@ import logging
 from eval import app
 import zmq
 
-def run_webapp(host=None,port=None):
+if __name__ != "__main__":
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+def run_webapp(host=None,port=None, **kwargs):
 
     zmq_client_context = zmq.Context()
     
@@ -10,17 +15,15 @@ def run_webapp(host=None,port=None):
     # print(" * Connecting to zmq server...")
     app.logger.info("Connecting to zmq server...")
     zmq_client_socket = zmq_client_context.socket(zmq.REQ)
-    zmq_client_socket.connect("tcp://localhost:5555")
-
+    zmq_client_socket.connect("tcp://%s:5555" % (kwargs["zmqserver_ip"],))        
     app.config['zmq_client_socket'] = zmq_client_socket
     if host and port:
         app.run(host=host, port=port)
+    else:
+        return app
 
-if __name__ != "__main__":
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
-    run_webapp()
+#if __name__ != "__main__":
+#    run_webapp()
     
 if __name__ == "__main__":
-    run_webapp(host='0.0.0.0', port=13000)
+    run_webapp(host='0.0.0.0', port=13000, zmqserver_ip="localhost")
